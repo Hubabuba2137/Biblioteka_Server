0fb9fd5d4ccb1ad2a530bdf6643c7a209b6183b6
Hubabuba2137 <117028372+Hubabuba2137@users.noreply.github.com>
2024-12-16 16:27:03 +0100

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,55 +1,24 @@
-﻿#include <crow.h>
+#include <crow.h>
 #include <iostream>
 #include <string>
 #include "Klasy.h"
+#include <unordered_map>
+#include <random>
 
-vector<string> get_rejestracja(string body) {
-	string imie;
-	string nazwisko;
-	string email;
-	string adres;
-	string tel;
-	string login;
-	string haslo;
-	int pos = 0;
+using namespace std;
 
-	imie = body.substr(9, body.length());
-	char c = 34;// 34 to kod ASCII znaku "
-	pos = login.find(c);
-	nazwisko = imie.substr(pos + 14, imie.length());
-	imie.substr(0, pos);
-	pos = nazwisko.find(c);
-	email = nazwisko.substr(pos + 14, nazwisko.length());
-	nazwisko.substr(0, pos);
-	pos = email.find(c);
-	adres = email.substr(pos + 11, email.length());
-	email.substr(0, pos);
-	pos = adres.find(c);
-	tel = stoi(adres.substr(pos + 13, adres.length()));
-	adres.substr(0, pos);
-	pos = tel.find(c);
-	login = tel.substr(pos + 11, tel.length());
-	tel.substr(0, pos);
-	pos = login.find(c);
-	haslo = login.substr(pos + 11, login.length());
-	login.substr(0, pos);
-	pos = haslo.find(c);
-	haslo.substr(0, pos);
-
-	vector<string> dane;
-	dane.push_back(imie);
-	dane.push_back(nazwisko);
-	dane.push_back(email);
-	dane.push_back(adres);
-	dane.push_back(tel);
-	dane.push_back(login);
-	dane.push_back(haslo);
-
-	return dane;
+std::string generateSessionID() {
+	static const char alphanum[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	std::string sessionID="";
+	for (int i = 0; i < 16; ++i) {
+		sessionID += alphanum[rand() % (sizeof(alphanum) - 1)];
+	}
+	return sessionID;
 }
 
 int main()
 {
+	srand(time(nullptr));
     crow::SimpleApp app;
 
 	BazaKsiazek baza_ksiazek;
@@ -61,14 +30,34 @@ int main()
 	baza_ksiazek.add("Lalka", "Bolesław Prus", 123);
 	baza_ksiazek.add("Quo vadis", "Henryk Sienkiewicz", 124);
 
-    CROW_ROUTE(app, "/")([]() {
-        auto page = crow::mustache::load_text("main.html");
-        return page;
+	std::unordered_map<std::string, std::string> sessions;
+
+    CROW_ROUTE(app, "/")([&sessions](const crow::request& req) {
+		std::string sessionID = req.get_header_value("Cookie");
+		std::string login = "";
+
+		if (!sessionID.empty() && sessions.count(sessionID)) {
+			login = sessions[sessionID];
+		}
+
+		auto page = crow::mustache::load("main.html");
+		crow::mustache::context ctx;
+		ctx["login"] = login; // Wstaw login do strony
+		return page.render(ctx); // Użyj renderowania strony
 		});
 
-    CROW_ROUTE(app, "/main.html")([]() {
-        auto page = crow::mustache::load_text("main.html");
-        return page;
+    CROW_ROUTE(app, "/main.html")([&sessions](const crow::request& req) {
+		std::string sessionID = req.get_header_value("Cookie");
+		std::string login = "";
+
+		if (!sessionID.empty() && sessions.count(sessionID)) {
+			login = sessions[sessionID];
+		}
+
+		auto page = crow::mustache::load("main.html");
+		crow::mustache::context ctx;
+		ctx["login"] = login; 
+		return page.render(ctx);
 		});
 
 	CROW_ROUTE(app, "/logowanie.html")
@@ -89,7 +78,31 @@ int main()
 		return page;
 			});
 
-	CROW_ROUTE(app, "/api/login").methods(crow::HTTPMethod::POST)([&baza_uzytkownikow](const crow::request& req) {
+	CROW_ROUTE(app, "/main_log.html")([&sessions](const crow::request& req) {
+		std::string sessionID = req.get_header_value("Cookie");
+		sessionID.erase(0, 10);//usuwamy prefiks sessionID=
+		std::string login;
+		
+		if (!sessionID.empty() && sessions.count(sessionID)) {
+			login = sessions[sessionID];
+		}
+
+		std::cout << "Login: " << login << std::endl;
+
+		auto page = crow::mustache::load("main_log.html");
+		crow::mustache::context ctx;
+		ctx["login"] = login; // Wstaw login do szablonu
+		return page.render(ctx); // Renderuj stronę z loginem
+		});
+
+	CROW_ROUTE(app, "/wyszukanie_log.html")
+		([](const crow::request& req) {
+		auto page = crow::mustache::load_text("wyszukanie_log.html");
+		return page;
+			});
+
+	CROW_ROUTE(app, "/api/login").methods(crow::HTTPMethod::POST)([&baza_uzytkownikow, &sessions](const crow::request& req) {\
+		//cout << req.body << endl;
 		auto body = req.body;
 		string login = body;
 		login.erase(0, 10);
@@ -103,15 +116,18 @@ int main()
 		haslo.erase(0, 11);
 		haslo.pop_back();
 		haslo.pop_back();
-
-		std::cout << login << std::endl;
-		std::cout << haslo << std::endl; 
 		
 		try {
 			if (baza_uzytkownikow.wyszukaj_czy_jest(login)) {
 				auto user = baza_uzytkownikow.wyszukaj(login);
 				if (user.checkPass(haslo)) {
-					return crow::response(200, "Poprawne logowanie");
+					std::string sessionID = generateSessionID();
+					sessions[sessionID] = login; // Zapisanie sesji w mapie
+					cout << "Zalogowano: " << login <<" w sesji: "<<sessionID<< endl;
+					crow::response res(200, "Poprawne logowanie");
+					res.add_header("Set-Cookie", "sessionID=" + sessionID + "; Path=/; HttpOnly");
+
+					return res;
 				}
 				else {
 					return crow::response(401, "Bledne haslo");
@@ -161,7 +177,7 @@ int main()
 		pos = haslo.find(c);
 		haslo = haslo.substr(0, pos);
 
-		std::cout << imie << ", " << nazwisko << ", " << email << ", " << adres << ", " << tel << ", " << login << ", " << haslo << std::endl;
+		//std::cout << imie << ", " << nazwisko << ", " << email << ", " << adres << ", " << tel << ", " << login << ", " << haslo << std::endl;
 		int telefon = std::stoi(tel);
 
 		try {
@@ -224,6 +240,51 @@ int main()
 		}
 		});
 
+	CROW_ROUTE(app, "/api/wyszukaj_log").methods(crow::HTTPMethod::POST)([&baza_ksiazek](const crow::request& req) {
+		auto body = crow::json::load(req.body);
+		if (!body) {
+			return crow::response(400, "Invalid JSON format");
+		}
+
+		std::string typ = body["typ"].s(); // "tytul" lub "isbn"
+		std::string wartosc = body["wartosc"].s();
+
+		try {
+			if (typ == "tytul") {
+				if (baza_ksiazek.wyszukaj_czy_jest(wartosc)) {
+					auto ksiazka = baza_ksiazek.wyszukaj((wartosc));
+					crow::json::wvalue result;
+					result["tytul"] = ksiazka.tytul;
+					result["autor"] = ksiazka.autor;
+					result["ISBN"] = ksiazka.ISBN;
+					return crow::response(result);
+				}
+				else {
+					return crow::response(404, "Nie znaleziono książki o podanym tytule");
+				}
+			}
+			else if (typ == "isbn") {
+				int isbn = std::stoi(wartosc);
+				if (baza_ksiazek.wyszukaj_czy_jest(isbn)) {
+					auto ksiazka = baza_ksiazek.wyszukaj(isbn);
+					crow::json::wvalue result;
+					result["tytul"] = ksiazka.tytul;
+					result["autor"] = ksiazka.autor;
+					result["ISBN"] = ksiazka.ISBN;
+					return crow::response(result);
+				}
+				else {
+					return crow::response(404, "Nie znaleziono książki o podanym ISBN");
+				}
+			}
+			else {
+				return crow::response(400, "Nieprawidłowy typ wyszukiwania");
+			}
+		}
+		catch (const std::exception& e) {
+			return crow::response(500, e.what());
+		}
+		});
 
     app.port(8080).multithreaded().run();
 }
